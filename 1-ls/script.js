e4cdc21fcb05ac19d1becf275cf9dbc845d8f20b
Imran <umurzakov117ali@gmail.com>
2022-06-15 11:37:04 +0500

    programming language concepts

---
+++ 1-ls/script.js
@@ -0,0 +1,47 @@
+/* MALUMOT TURLARI */
+/**/
+//
+
+// STRING
+// console.log('hello world');
+// console.log("hello world");
+// console.log(`hello world`);
+
+// NUMBER
+
+// console.log(10);
+// console.log(10.5);
+
+// BOOLEAN
+
+// console.log(true);
+// console.log(false);
+
+// BIGINT
+
+// console.log(9684613215954256546544n);
+
+/*O'ZGARUVCHILAR*/
+
+// var ism = 'Max',
+//   age = 45,
+//   work = true;
+
+// console.log(ism);
+// console.log(age);
+// console.log(work);
+
+// var num = null;
+
+// console.log(num);
+
+// num = 1231;
+
+// console.log(num);
+
+// var myName = 'Max';
+// var myName = 'Max';
+
+let myName;
+let myname;
+
