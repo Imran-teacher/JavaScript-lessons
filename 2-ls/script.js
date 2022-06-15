b1bca7ee094852de13914bfc9f0f7293265f0490
Imran-teacher <107421792+Imran-teacher@users.noreply.github.com>
2022-06-15 11:39:13 +0500

    working with if-else, switch Case

---
+++ 2-ls/script.js
@@ -0,0 +1,149 @@
+// Arifmetik operatorlar
+
+// console.log(5 + 5);
+// console.log(20 - 5);
+// console.log(20 * 5);
+// console.log(20 / 5);
+// console.log(20 % 7);
+
+// Konkatenatsiya matni matnga qoshish
+
+// let result1 = 'salom ';
+// let result2 = 'dunyo';
+
+// let answer = result1 + result2;
+
+// console.log(answer);
+
+// console.log('salom ' + (15 + 1));
+
+// console.log('salom' + true);
+
+// console.log(true + true);
+
+// && - va
+
+// console.log(true && true); // true
+// console.log(1 && true); // true
+// console.log(200 && 100); // 200
+
+// || - yoki
+
+// console.log(true || false); // true
+// console.log(10 || 20); // 10
+// console.log('' || 'salom'); // salom
+
+// ! - emas
+
+// console.log(!false); // true
+// console.log(!true); // false
+
+// == === - taqqoslash
+
+// console.log('10' == 10); //  true
+// console.log('10' === 10); // false
+// console.log(true === 1); // false
+
+// console.log(10 != '10');  // false
+// console.log(10 !== '10'); // true
+
+// > < >=, <=,
+
+// console.log(10 >= 10);
+
+// IF-ELSE
+
+// let num1 = 20;
+// let num2 = 5;
+
+// if (num1 == num2) {
+//   console.log('teng');
+// }else if(num1 == num2 + 10) {
+//   console.log('10ga katta qildik');
+// }else if(num1 == num2 + 15) {
+//   console.log('15ga katta qildik');
+// }
+
+// if(num1 == num2 + 10) {
+//   console.log('ikkinchi if ishladi');
+// }
+
+// console.log(+'15' + 10);
+// prompt() userdan malumot olish uchun kerak boladi
+
+// let namee = prompt('iltmos ismingizni kiriting');
+// let age = +prompt('iltmos yoshingizni kiriting');
+// console.log(age);
+
+// let yosh = +prompt('iltmos yoshingizni kiriting');
+
+// if(yosh == 0 || yosh < 0 || isNaN(yosh)) {
+//   alert('oqishni organ ukam');
+// }else if (yosh >= 1 && yosh <= 18) {
+//   alert('siz hali oqishingiz kerak')
+// }else if (yosh > 18 && yosh <= 23) {
+//   alert('siz ishlashingiz kerak')
+// }else if (yosh > 23 && yosh <= 36) {
+//   alert('uylanish kerak')
+// }else if (yosh > 36 && yosh <= 50) {
+//   alert('pensiyaga 300 min qoshib berishdimi ?')
+// }else if (yosh > 50 && yosh <= 100) {
+//   alert('nevarangizni oqiting')
+// }
+
+// let num1 = 10;
+
+// switch (num1) {
+//   case '10':
+//     console.log('ha bu string ekan');
+//     break;
+//   case 10:
+//     console.log('ha bu teng va son ekan');
+//     break;
+//   case  5:
+//     console.log('yoq uni qiymati son 5 ekan');
+//     break;
+//   default:
+//     console.log('notog`ri');
+//     break;
+// }
+
+
+// ++ - bir qoshish inkrement
+// -- - bir ayirish dekrement
+
+// i++ - postfiks keyin qoshadi
+// ++i - prefiks oldin qoshadi
+
+// let i = 5;
+
+// console.log(i++); // 5
+
+// console.log(++i); // 7
+
+// let result = 'salom'
+
+// console.log(++result);
+
+// NaN - uninue symbol
+
+console.log(NaN == NaN);
+
+// 1-uy ishi 
+/*
+User kiradi va ismi bilan yoshini kiritadi Javob esa consolega chiqishi kerak boladi
+
+Javob: Ismi Ivan, yoshi 50 da
+
+2- uy ishi
+User 3ta son kiritadi va ulardan orta sonni topish kerak boladi Javob alertga chiqishi kerak boladi
+
+let num1 = +prompt('1 son kiriting') 10
+let num2 = +prompt('2 son kiriting') 15
+let num3 = +prompt('3 son kiriting') 20
+
+3- uy ishi
+User son kiritadi va u sonni juft yoki toq sonligini bilib berish kerak boladi javob alertga chiqishi kerak
+
+*/
+
